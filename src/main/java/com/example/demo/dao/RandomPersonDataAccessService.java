4a7e9d2b9153a3cac89560bb6159614e4ed6ff9d
p-papag <thedrpapa@gmail.com>
2020-08-17 14:16:08 +0300

    First commit

---
+++ src/main/java/com/example/demo/dao/RandomPersonDataAccessService.java
@@ -0,0 +1,60 @@
+package com.example.demo.dao;
+
+import com.example.demo.model.Person;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository("randomDao")
+
+public class RandomPersonDataAccessService implements IPersonDao {
+
+    private static List<Person> db = new ArrayList<>();
+
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        db.add(new Person(id, person.getName()));
+        return 1;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return db;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return db.stream()
+                .filter(person -> person.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        Optional<Person> personExists = selectPersonById(id);
+        if(personExists.isEmpty()) {
+            return 0;
+
+        }
+        db.remove(personExists.get());
+        return 1;
+
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return selectPersonById(id)
+                .map(person1 -> {
+                    int indexOfPersonToUpdate = db.indexOf(person);
+                    if(indexOfPersonToUpdate >= 0) {
+                        db.set(indexOfPersonToUpdate, person);
+                        return 1;
+                    }
+                    return 0;
+                })
+                .orElse(0);
+    }
+}
